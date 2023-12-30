14b63580e26376756700b58acb16d49ff9c535ac
zhumaohua <13126959424@163.com>
2023-12-30 14:37:50 +0800

    skip wallets with too many unconfirmed utxos

--- lib/utils/go-worker.ts
+++ lib/utils/go-worker.ts
@@ -2,12 +2,17 @@ import { spawn } from 'child_process';
 import { promisify } from 'util';
 import * as stream from 'stream';
 const pipeline = promisify(stream.pipeline);
+import * as chalk from 'chalk';
 
 // 封装Go调用逻辑
 export async function callGoProgram(bin, data) {
     const goProcess = spawn(bin);
     goProcess.stderr.on('data', (data) => {
-        console.error(`${data}`);
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(
+            chalk.green(data)
+        );
     });
 
     // 创建一个可写流，以便我们可以发送数据到Go程序
